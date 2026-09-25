5e5f47cd4e5c6458450093d1ddb85f99c5db25bc
arshad <syedarshad0756@gmail.com>
2026-09-25 20:20:33 +0530

    Initial loan approval prediction project

---
+++ app.py
@@ -0,0 +1,91 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+
+# Load model
+model = joblib.load("model.pkl")
+
+
+from supabase import create_client
+
+SUPABASE_URL = "YOUR_SUPABASE_URL"
+SUPABASE_KEY = "YOUR_SUPABASE_KEY"
+
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
+
+
+st.set_page_config(
+    page_title="Loan Approval",
+    page_icon="💰"
+)
+
+
+st.title("💰 Loan Approval Prediction")
+st.write("Enter applicant details")
+
+
+age = st.number_input("Age")
+
+income = st.number_input("Income")
+
+credit_score = st.number_input(
+    "Credit Score"
+)
+
+loan_amount = st.number_input(
+    "Loan Amount"
+)
+
+loan_term = st.number_input(
+    "Loan Term (Months)"
+)
+
+employment = st.selectbox(
+    "Employment Type",
+    [" ","Salaried", "Self-employed"]
+)
+
+existing_loans = st.number_input(
+    "Existing Loans")
+
+
+
+if st.button("Check Loan Approval"):
+
+    new_applicant = pd.DataFrame({
+        "Age": [age],
+        "Income": [income],
+        "Credit_Score": [credit_score],
+        "Loan_Amount": [loan_amount],
+        "Loan_Term": [loan_term],
+        "Employment_Type": [employment],
+        "Existing_Loans": [existing_loans],
+    })
+
+    # Model prediction
+    result = model.predict(new_applicant)
+
+    # Convert prediction to True/False
+    approved = result[0] == 1
+
+    # Save to Supabase
+    supabase.table("loan_app").insert({
+        "age": age,
+        "income": income,
+        "credit_score": credit_score,
+        "loan_amount": loan_amount,
+        "loan_term": loan_term,
+        "employment_type": employment,
+        "existing_loans": existing_loans,
+        "loan_approved": approved
+    }).execute()
+
+    # Display result
+    if approved:
+        st.success("✅ Loan Approved")
+    else:
+        st.error("❌ Loan Not Approved")
---
+++ trained.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+
+
+
+# Load data
+df = pd.read_csv("loan_approval_100_records.csv")
+
+
+# Features
+X = df[
+    [
+        "Age",
+        "Income",
+        "Credit_Score",
+        "Loan_Amount",
+        "Loan_Term",
+        "Employment_Type",
+        "Existing_Loans",
+    ]
+]
+
+
+# Target
+y = df["Loan_Approved"]
+
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+
+# Columns
+categorical_columns = ["Employment_Type"]
+
+numerical_columns = [
+    "Age",
+    "Income",
+    "Credit_Score",
+    "Loan_Amount",
+    "Loan_Term",
+    "Existing_Loans"
+]
+
+
+# Preprocessing
+preprocessor = ColumnTransformer(
+    transformers=[
+        (
+            "categorical",
+            OneHotEncoder(handle_unknown="ignore"),
+            categorical_columns
+        ),
+        (
+            "numerical",
+            StandardScaler(),
+            numerical_columns
+        )
+    ]
+)
+
+
+# Pipeline
+model = Pipeline([
+    ("preprocessing", preprocessor),
+    ("logistic_regression", LogisticRegression(max_iter=1000))
+])
+
+
+# Train
+model.fit(X_train, y_train)
+
+
+# Save
+joblib.dump(model, "model.pkl")
+
+print("Model saved successfully!")
